66d3ad0efc257c930fdec04cd26c7dee424147ff
JaCuenca <jordan_cuenca@hotmail.es>
2020-05-30 22:30:31 -0500

    Nuevo metodo para realizar las operaciones

--- app/src/main/java/com/example/calculadorabasica/MainActivity.java
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -11,4 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String calcular(float num1, float num2, char operacion){
+        switch (operacion){
+            case '+': return String.valueOf(num1+num2);
+            case '-': return String.valueOf(num1-num2);
+            case '*': return String.valueOf(num1*num2);
+            case '/': return String.valueOf(num1/num2);
+            default: return null;
+        }
+    }
 }
