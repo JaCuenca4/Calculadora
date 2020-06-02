a8953ba617137dd35ba0941403498d0847c4eebd
cristianbc1 <38725484+cristianbc1@users.noreply.github.com>
2020-06-01 23:54:54 -0500

    Unit Test

---
+++ app/src/test/java/com/example/calculadorabasica/Model/ModelMainActivityTest.java
@@ -0,0 +1,24 @@
+package com.example.calculadorabasica.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModelMainActivityTest {
+
+    @Test
+    public void sumar() {
+    }
+
+    @Test
+    public void restar() {
+    }
+
+    @Test
+    public void multiplicar() {
+    }
+
+    @Test
+    public void dividir() {
+    }
+}
