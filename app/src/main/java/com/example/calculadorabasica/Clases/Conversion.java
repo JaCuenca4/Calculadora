b454867500afe4ee2f29de6fd2cccd499f898d84
cristianbc1 <38725484+cristianbc1@users.noreply.github.com>
2020-06-16 14:24:42 -0500

    Funciones convertir

--- app/src/main/java/com/example/calculadorabasica/Clases/Conversion.java
+++ app/src/main/java/com/example/calculadorabasica/Clases/Conversion.java
@@ -12,43 +12,52 @@ public class Conversion {
     public List<String> convertir(String dato, String identificador){
         List<String> resultados = new ArrayList<>();
 
-        resultados.add(getHexadecimal(dato,identificador));
-        resultados.add(getDecimal(dato,identificador));
-        resultados.add(getOctal(dato,identificador));
-        resultados.add(getBinario(dato,identificador));
+        dato=convertDecimal(dato,identificador);
+        resultados.add(getHexa(dato));
+        resultados.add(getDecimal(dato));
+        resultados.add(getOctal(dato));
+        resultados.add(getBinario(dato));
 
         return null;
     }
-
-    public String getHexadecimal(String dato,String identificador){
-        //Codigo para convertir
+    public String convertDecimal(String dato, String identificador){
         if(identificador.equals("HEX")){
-            return dato;
-        }else if(identificador.equals("DEC")){
-            int decimal=Integer.parseInt(dato,16);
+            int decimal = Integer.parseInt(dato, 16);
             return String.valueOf(decimal);
-        }else if(identificador.equals("OCT")){
-            return null;
+        }else if(identificador.equals("DEC")){
+            return dato;
         }else if(identificador.equals("BIN")){
-            return null;
+            long numeroDecimal = Long.parseLong(dato, 2);
+            return String.valueOf(numeroDecimal);
+        }else if(identificador.equals("OCT")){
+            int decimal=Integer.parseInt(dato,8);
+            return String.valueOf(decimal);
         }else{
-            return null;
+            return dato;
         }
+    }
+    public String getHexa(String dato){
+        int dato2=Integer.getInteger(dato);
+        String hexadecimal = Integer.toHexString(dato2);
+       return hexadecimal;
 
     }
 
-    public String getDecimal(String dato, String identificador){
-        //Codigo para convertir
-        return null;
+    public String getDecimal(String dato){
+        return dato;
     }
 
-    public String getOctal(String dato,String identificador){
-        //Codigo para convertir
-        return null;
+    public String getOctal(String dato){
+        int dato2=Integer.getInteger(dato);
+        String octal = Integer.toOctalString(dato2);
+
+
+        return octal;
     }
 
-    public String getBinario(String dato,String identificador){
-        //Codigo para convertir
-        return null;
+    public String getBinario(String dato){
+        int dato2=Integer.getInteger(dato);
+        String binario = Long.toBinaryString(dato2);
+        return binario;
     }
 }
